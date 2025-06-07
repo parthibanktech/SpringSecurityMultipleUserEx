c77c41ac2fd2d5c11fcb84a2b9aa66715d5a1a6a
parthibanktech <parthiban.k.tech3@gmail.com>
2025-06-07 13:50:40 +0700

    Added Hello World print statement in DemoApplication 11

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,8 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Hello World");
-
+		System.out.println("Hello World 1 12");
+ 
 	}
 
 }
